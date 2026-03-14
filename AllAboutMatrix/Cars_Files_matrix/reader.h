e6710bb3b9427c1f6499d4586f97f24006bb5be7
root <root@DESKTOP-GNNSMQ4.localdomain>
2026-03-14 03:43:33 +0300

    Cars from text

---
+++ AllAboutMatrix/Cars_Files_matrix/reader.h
@@ -0,0 +1,16 @@
+#ifndef READER_H
+#define READER_H
+#include <fstream>
+
+class Reader
+{
+    private:
+        std::ifstream m_in;
+    public:
+        Reader(const std::string& file);
+        ~Reader();
+
+        bool isOpen();
+};
+
+#endif
